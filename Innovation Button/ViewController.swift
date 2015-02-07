972776d3afc8ec573daef2cf0581887eca520ea1
Chris Neigh <chris@Chriss-MBP.home>
2015-02-06 23:43:56 -0500

    Initial Commit

---
+++ Innovation Button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Innovation Button
+//
+//  Created by Chris Neigh on 2/6/15.
+//  Copyright (c) 2015 Chris Neigh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
