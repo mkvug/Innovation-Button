073a3f95dbeae1dd777cc87dc0e015e2037dd461
Chris Neigh <chris@Chriss-MBP.home>
2015-02-07 23:25:07 -0500

    initial commit

--- Innovation Button/ViewController.swift
+++ Innovation Button/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var innovativeIdeas: UILabel!
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    let innovationSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("innovation", ofType: "mp3")!)
+    let url = NSURL(string: "http://www.innovationbutton.com/?innovation=1")
+    let countURL = NSURL(string: "http://www.innovationbutton.com/json")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: innovationSound, error: nil)
+        audioPlayer.prepareToPlay()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func playInnovationClip(sender: AnyObject) {
+        audioPlayer.play()
+        
+        let request = NSURLRequest(URL: url!)
+        let connection = NSURLConnection(request: request, delegate:nil, startImmediately: true)
+        let countRequest = NSURLRequest(URL: countURL!)
 
-
+        NSURLConnection.sendAsynchronousRequest(countRequest, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            let jsonObject : AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+            let json = JSON(data: data)
+            let count = json["count"].stringValue
+            
+            self.innovativeIdeas.text = "\(count) Innovative Ideas"
+        }
+    }
 }
 
